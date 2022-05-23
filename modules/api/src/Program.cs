78219d7284d083534d4f32198d87923d04f6a46d
Lee Wortley <lee.wortley@madetech.com>
2022-05-23 10:37:06 +0100

    Adding Json Logging to project ready for integration with splunk

--- modules/api/src/Program.cs
+++ modules/api/src/Program.cs
@@ -2,6 +2,9 @@ using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using NLog;
 
 namespace gpconnect_user_portal.api
 {
@@ -9,7 +12,25 @@ namespace gpconnect_user_portal.api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                //NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+        
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) {
@@ -25,7 +46,13 @@ namespace gpconnect_user_portal.api
                     builder.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
                     builder.AddEnvironmentVariables();
                     // More config here
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog();
         }
     }
 }
