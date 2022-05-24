db08725819474b1985ecb02de38075505101944f
Lee Wortley <lee.wortley@madetech.com>
2022-05-24 13:42:30 +0100

    removing unused import

--- modules/api/src/Startup.cs
+++ modules/api/src/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace GpConnect.NationalDataSharingPortal.Api
 {
