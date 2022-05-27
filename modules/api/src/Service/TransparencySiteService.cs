2ebe05c22c25f05c792d1f668743549d81e5efbd
Lee Wortley <lee.wortley@madetech.com>
2022-05-27 16:48:39 +0100

    fix: Updated the TransparencySiteService to inject typed Logger, updated the appseetings.Development.json to work with the docker-compose database definition

--- modules/api/src/Service/TransparencySiteService.cs
+++ modules/api/src/Service/TransparencySiteService.cs
@@ -15,10 +15,16 @@ namespace GpConnect.NationalDataSharingPortal.Api.Service
         private enum SiteStatus {
             LIVE = 5,
         }
-        private readonly IDataService _dataService;
-        private readonly ILogger _logger;
 
-        public TransparencySiteService(IDataService dataService, ILogger logger)
+        public const string HtmlQueryFilterInteraction = "gpc.getcarerecord";
+        public const string StructuredQueryFilterInteraction = "structured:fhir:rest:read:metadata-1";
+        public const string AppointmentQueryFilterInteraction = "appointments-1";
+        public const string SendDocumentQueryFilterInteraction = "documents:fhir:rest:read:metadata-1";        
+
+        private readonly IDataService _dataService;
+        private readonly ILogger<TransparencySiteService> _logger;
+
+        public TransparencySiteService(IDataService dataService, ILogger<TransparencySiteService> logger)
         {
             _dataService = dataService;
             _logger = logger;
@@ -28,7 +34,8 @@ namespace GpConnect.NationalDataSharingPortal.Api.Service
         {
             var query = "application.find_sites";
 
-            var parameters = new DynamicParameters();
+            var parameters = BuildBlankSearchParameters();
+
             parameters.Add("_site_definition_status_min", SiteStatus.LIVE, DbType.Int16, ParameterDirection.Input);
             parameters.Add("_site_definition_status_max", SiteStatus.LIVE, DbType.Int16, ParameterDirection.Input);
 
@@ -36,14 +43,34 @@ namespace GpConnect.NationalDataSharingPortal.Api.Service
             {
                 _logger.LogDebug("Preparing Query for ODS based search, {}", request.ProviderCode);
                 parameters.Add("_site_ods_code", request.ProviderCode, DbType.String, ParameterDirection.Input);
+                parameters.Add("_site_name", null, DbType.String, ParameterDirection.Input);
             }
             else
             {
                 _logger.LogDebug("Preparing Query for name based search, {}", request.ProviderName);
                 parameters.Add("_site_name", request.ProviderName, DbType.String, ParameterDirection.Input);
+                parameters.Add("_site_ods_code", null, DbType.String, ParameterDirection.Input);
             }
-         
+
             return _dataService.ExecuteQuery<TransparencySite>(query, parameters);        
+        }
+
+        private DynamicParameters BuildBlankSearchParameters()
+        {
+            var parameters = new DynamicParameters();
+            
+            // Add blank filters as we are not interested in filtering
+            parameters.Add("_filter_by", 0, DbType.Int16, ParameterDirection.Input);
+            parameters.Add("_html_query_filter_interaction", HtmlQueryFilterInteraction, DbType.String, ParameterDirection.Input);
+            parameters.Add("_structured_query_filter_interaction", StructuredQueryFilterInteraction, DbType.String, ParameterDirection.Input);
+            parameters.Add("_appointment_query_filter_interaction", AppointmentQueryFilterInteraction, DbType.String, ParameterDirection.Input);
+            parameters.Add("_send_document_query_filter_interaction", SendDocumentQueryFilterInteraction, DbType.String, ParameterDirection.Input);            
+
+            // Blank off the ccg fields as not searching by this in MVP
+            parameters.Add("_ccg_name", null, DbType.String, ParameterDirection.Input);
+            parameters.Add("_ccg_ods_code", null, DbType.String, ParameterDirection.Input);
+
+            return parameters;
         }
     }
 }
