fe5c095784c15862dda90b8cfecb6f22f7188c03
Adrian Wilkins <adrian.wilkins@madetech.com>
2022-06-05 17:34:40 +0100

    Fix the error page not working in the admin portal

--- source/gpconnect-user-portal.Admin/Pages/Error.cshtml.cs
+++ source/gpconnect-user-portal.Admin/Pages/Error.cshtml.cs
@@ -1,23 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 using System.Diagnostics;
+
+using gpconnect_user_portal.Services.Interfaces;
 
 namespace gpconnect_user_portal.Admin.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
-    public class ErrorModel : PageModel
+    public class ErrorModel : BaseModel
     {
+
+        public ErrorModel(ILogger<ErrorModel> logger, IAggregateService aggregateService, IOptionsMonitor<DTO.Response.Configuration.General> generalOptionsDelegate) : base(aggregateService, generalOptionsDelegate)
+        {
+
+        }
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-        private readonly ILogger<ErrorModel> _logger;
-
-        public ErrorModel(ILogger<ErrorModel> logger)
-        {
-            _logger = logger;
-        }
 
         public void OnGet()
         {
