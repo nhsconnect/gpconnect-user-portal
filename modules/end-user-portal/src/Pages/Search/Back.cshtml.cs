4ee673a6d1510608a491467ac514df348ac2345c
Lee Wortley <lee.wortley@madetech.com>
2022-06-21 14:54:50 +0100

    Refactored the Back functionality to be in code rather than cshtml files. Added Unit Tests

--- modules/end-user-portal/src/Pages/Search/Back.cshtml.cs
+++ modules/end-user-portal/src/Pages/Search/Back.cshtml.cs
@@ -10,4 +10,27 @@ public class BackPartialModel
     public int ResultsPageNumber { get; set; } = 1;
 
     public string PageName => Source == DetailViewSource.Results ? "/Search/Results" : $"/Search/{Mode}";
+    public IDictionary<string, string> Params => GenerateRouteParamsDictionary();
+    private IDictionary<string, string> GenerateRouteParamsDictionary()
+    {
+        if (Source == DetailViewSource.Search)
+        {
+            return new Dictionary<string,string> 
+            {
+                { ParamNameForMode(Mode), Query }
+            };
+        }
+
+        return new Dictionary<string,string>
+        {
+            { "Query", Query },
+            { "Mode", Mode.ToString() },
+            { "PageNumber", ResultsPageNumber.ToString() }
+        };
+    }
+
+    private string ParamNameForMode(SearchMode mode)
+    {
+        return mode == SearchMode.Code ? "providerOdsCode" : "providerName";
+    }
 }
