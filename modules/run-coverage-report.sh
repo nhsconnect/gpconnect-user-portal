cb08120bc40fcaf2ce7af9edd20d873902e7bf1b
Lee Wortley <lee.wortley@madetech.com>
2022-06-22 09:00:53 +0100

    Updating the report script to use common variables rather than raw paths

--- modules/run-coverage-report.sh
+++ modules/run-coverage-report.sh
@@ -1,22 +1,35 @@
 REPORTS=""
+INPUT_LOCATION=./coverage-input
 echo "Cleaning old Coverage Reports"
 rm -rf coveragereport
 echo "Generating Coverage Reports"
-mkdir input
-echo "Testing API..."
-dotnet test api --collect:"XPlat Code Coverage"
-mv ./api/test/TestResults/*/coverage* ./input
-mv ./input/coverage.cobertura.xml ./input/api-report.xml
-rm -rf ./api/test/TestResults
-REPORTS="./input/api-report.xml"
+mkdir $INPUT_LOCATION
 
 echo "Testing API..."
+dotnet test api --collect:"XPlat Code Coverage"
+mv ./api/test/TestResults/*/coverage* ./$INPUT_LOCATION
+mv ./$INPUT_LOCATION/coverage.cobertura.xml ./$INPUT_LOCATION/api-report.xml
+rm -rf ./api/test/TestResults
+
+REPORTS="./$INPUT_LOCATION/api-report.xml"
+
+echo "Testing UI..."
 dotnet test end-user-portal --collect:"XPlat Code Coverage"
-mv ./end-user-portal/gpconnect-user-portal.test/TestResults/*/coverage* ./ui-report.xml
+mv ./end-user-portal/gpconnect-user-portal.test/TestResults/*/coverage* ./$INPUT_LOCATION
+mv ./$INPUT_LOCATION/coverage.cobertura.xml ./$INPUT_LOCATION/ui-report.xml
 rm -rf ./end-user-portal/gpconnect-user-portal.test/TestResults
-REPORTS=${REPORTS}";./ui-report.xml"
+
+REPORTS=${REPORTS}";./$INPUT_LOCATION/ui-report.xml"
+
+# echo "Testing Admin Portal..."
+# dotnet test end-user-portal --collect:"XPlat Code Coverage"
+# mv ./end-user-portal/gpconnect-user-portal.test/TestResults/*/coverage* ./input
+# mv ./input/coverage.cobertura.xml ./input/admin-report.xml
+# rm -rf ./end-user-portal/gpconnect-user-portal.test/TestResults
+
+# REPORTS=${REPORTS}";./input/admin-report.xml"
 
 echo "Generate HTML Report"
 reportgenerator -reports:"${REPORTS}" -targetdir:"coveragereport" -reporttypes:Html
 
-open ./coveragereport/Index.html
+rm -rf $INPUT_LOCATION && open ./coveragereport/Index.html
