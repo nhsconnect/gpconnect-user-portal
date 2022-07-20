382b5c0190baa3466a76afad3ac3a9037822df9d
Lee Wortley <lee@wyamee.com>
2022-07-20 11:46:24 +0100

    Fix/api rejecting post request (#63)
    Updating the Database Schema to have the SiteDefinition Statuses. 
    Updating migrator to use secret rather than provided password
    
    Correcting bad path in Api documentation
    
    Co-authored-by: Lee Wortley <lee.wortley@madetech.com>
    Co-authored-by: Adrian Wilkins <adrian.wilkins3@nhs.net>

---
+++ database/get_secret_and_migrate.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -eux
+
+SECRET_ARN=$1
+
+read -r FLYWAY_USER FLYWAY_PASSWORD <<< "$(
+  aws secretsmanager get-secret-value \
+    --secret-id "${SECRET_ARN}" \
+    --query 'SecretString' \
+    --output text | jq -r '[.username, .password] | @tsv'
+)"
+
+export FLYWAY_USER
+export FLYWAY_PASSWORD
+
+flyway -connectRetries=10 migrate
--- modules/api/service-level-tests/db_data_source/test_data_source.sql
+++ modules/api/service-level-tests/db_data_source/test_data_source.sql
@@ -17,16 +17,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Data for Name: site_definition_status; Type: TABLE DATA; Schema: application; Owner: postgres
---
-
-INSERT INTO application.site_definition_status (site_definition_status_id, site_definition_status_name) VALUES (1, 'Draft');
-INSERT INTO application.site_definition_status (site_definition_status_id, site_definition_status_name) VALUES (2, 'Awaiting Review');
-INSERT INTO application.site_definition_status (site_definition_status_id, site_definition_status_name) VALUES (3, 'Awaiting Spine Update');
-INSERT INTO application.site_definition_status (site_definition_status_id, site_definition_status_name) VALUES (4, 'Completed');
-INSERT INTO application.site_definition_status (site_definition_status_id, site_definition_status_name) VALUES (5, 'Live');
-
---
 -- Data for Name: site_definition; Type: TABLE DATA; Schema: application; Owner: postgres
 --
 
