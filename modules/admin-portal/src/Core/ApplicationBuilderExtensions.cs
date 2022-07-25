e32846deee12d041ef6171998114f4b887d6231b
Lee Wortley <lee.wortley@madetech.com>
2022-07-25 10:49:49 +0100

    feat: Initial working application with logging and relevant cookies.

---
+++ modules/admin-portal/src/Core/ApplicationBuilderExtensions.cs
@@ -0,0 +1,71 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
+
+namespace GpConnect.NationalDataSharingPortal.AdminPortal.Core;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ConfigureApplicationBuilderServices(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedProto
+        });
+
+        if (!env.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            OnPrepareResponse = context =>
+            {
+                context.Context.Response.Headers[HeaderNames.CacheControl] = $"public, max-age={TimeSpan.FromSeconds(60 * 60 * 24)}";
+            }
+        });
+        
+        app.UseSession();
+        app.UseCookiePolicy();
+        app.UseRouting();
+        app.UseResponseCaching();
+
+        app.Use(async (context, next) =>
+        {
+            context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue()
+            {
+                NoStore = true,
+                NoCache = true
+            };
+            await next();
+        });       
+
+        app.UseResponseCompression();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+            endpoints.MapHealthChecks("/healthcheck", new HealthCheckOptions()
+            {
+                ResultStatusCodes =
+                      {
+                        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                      },
+                AllowCachingResponses = false
+            });
+        });
+    }
+}
